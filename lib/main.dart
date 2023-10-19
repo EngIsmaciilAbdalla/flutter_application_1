e1d48ec56e8c8390f4106ed38c0f97e4973b5ddf
IsmaciilAbdalla <ismaciilcabdalla57@gmail.com>
2023-10-19 13:05:57 -0800

    KUCELIS

--- lib/main.dart
+++ lib/main.dart
@@ -14,78 +14,353 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 219, 241, 112),
+      backgroundColor: const Color.fromARGB(255, 13, 33, 49),
       body: SafeArea(
-        child: Column(
-          children: [
-            Card(
-              margin: EdgeInsets.all(5),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(45)),
-              elevation: 10,
-              child: Image(image: AssetImage("images/faarax.jpg")),
+        child: Column(children: [
+          ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(200)),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 206, 191, 191),
+              ),
+              margin: EdgeInsets.all(
+                10,
+              ),
+              width: 400,
+              height: 160,
+              padding: EdgeInsets.all(10),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(200),
+                  child: Image(image: AssetImage('images/faarax.jpg'))),
             ),
-            SizedBox(
-              height: 20,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          //Cards
+          //Card 1
+          Card(
+            color: Color.fromARGB(255, 206, 191, 191),
+            elevation: 10,
+            shadowColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(57),
             ),
-            Card(
-              margin: EdgeInsets.all(5),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25)),
-              elevation: 10,
-              child: Row(children: [
+            child: Row(
+              children: [
                 CircleAvatar(
-                  radius: 30,
+                  radius: 45,
                   backgroundImage: AssetImage('images/faarax.jpg'),
                 ),
-                Container(
-                  margin: EdgeInsets.only(left: 25),
-                  child: Column(
-                    children: [
-                      Text(
-                        "Pizza Large meat",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20),
-                      ),
-                      Text(
-                        "Availible",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16),
-                      ),
-                      Container(
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.star,
-                              color: Colors.green,
-                            ),
-                            Icon(Icons.star, color: Colors.green),
-                            Icon(Icons.star, color: Colors.green),
-                            Icon(Icons.star, color: Colors.green),
-                            Icon(Icons.star, color: Colors.green),
-                            Icon(Icons.star, color: Colors.green)
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                SizedBox(
+                  width: 15,
                 ),
-                Container(
-                  margin: EdgeInsets.only(left: 25),
-                  child: Row(
-                    children: [
-                      Text(
-                        "13",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20),
+                Column(
+                  children: [
+                    Text(
+                      "Pizza Large Meat",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: 70),
+                      child: Text(
+                        "Available",
+                        style: TextStyle(fontSize: 20),
                       ),
-                    ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  width: 34,
+                ),
+                Text(
+                  "\$ 13",
+                  style: TextStyle(
+                    fontSize: 25,
                   ),
                 )
-              ]),
+              ],
             ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          //Card 2
+          Card(
+            color: Color.fromARGB(255, 206, 191, 191),
+            elevation: 10,
+            shadowColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(57),
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 45,
+                  backgroundImage: AssetImage('images/faarax.jpg'),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "Pizza Large Meat",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: 70),
+                      child: Text(
+                        "Available",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  width: 34,
+                ),
+                Text(
+                  "\$ 14",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          // Card 3
+          Card(
+            color: Color.fromARGB(255, 206, 191, 191),
+            elevation: 10,
+            shadowColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(57),
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 45,
+                  backgroundImage: AssetImage('images/faarax.jpg'),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "Pizza Large Meat",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: 70),
+                      child: Text(
+                        "Available",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  width: 34,
+                ),
+                Text(
+                  "\$ 11",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          // Card 4
+          Card(
+            color: Color.fromARGB(255, 206, 191, 191),
+            elevation: 10,
+            shadowColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(57),
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage('images/faarax.jpg'),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "Pizza Large Meat",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: 70),
+                      child: Text(
+                        "Available",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 44, 206, 12),
+                          size: 25,
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  width: 34,
+                ),
+                Text(
+                  "\$ 15",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+
+          //END OF CARDS
+        ]),
       ),
     );
   }
